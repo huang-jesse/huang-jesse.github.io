d04b3448c2c8b4ab1f5217eb7a9f36404d097900
huang-jesse <huang-jesse@outlook.com>
2024-07-25 19:03:20 +0800

    Site updated: 2024-07-25 19:03:19

--- js/googletagmanager.js
+++ js/googletagmanager.js
@@ -1,10 +1,7 @@
-(function (w, d, s, l, i) {
-w[l] = w[l] || [];
-w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-j.async = true;
-j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i + dl;
-f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'G-S05CZE167X');
+// GA4
+const ga_token = 'G-S05CZE167X'
+script(async='', src='https://www.googletagmanager.com/gtag/js?id=' + ga_token)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', ga_token);
