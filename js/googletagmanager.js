6c09ebc667d573574b57bbe6b57167e9a4ac295e
huang-jesse <huang-jesse@outlook.com>
2024-07-25 19:17:32 +0800

    Site updated: 2024-07-25 19:17:30

--- js/googletagmanager.js
+++ js/googletagmanager.js
@@ -1,10 +1,11 @@
 // GA4
+// add js to head
 const ga_token = 'G-S05CZE167X'
 let script = document.createElement('script');
 script.src = "https://www.googletagmanager.com/gtag/js?id=" + ga_token;
-document.body.append(script);
 script.async = true;
-// script(async='', src='https://www.googletagmanager.com/gtag/js?id=' + ga_token)
+document.head.append(script);
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
